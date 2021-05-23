9bdab50d0da18d61a396a36067d3e7816d6f12da
swarup4 <swarup.saha004@hotmail.com>
2021-05-24 02:03:10 +0530

    Build Custom action

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -11,18 +11,14 @@
 #
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
-from rasa_sdk.executor import CollectingDispatcher
 
+class ActionCheckWeather(Action):
+   def name(self):
+      return "action_check_weather"
 
-class ActionHelloWorld(Action):
+   def run(self, dispatcher,
+           tracker: Tracker,
+           domain):
 
-    def name(self) -> Text:
-        return "action_hello_world"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
-
-        return []
+      dispatcher.utter_message("Hello World! from custom action")
+      return []
